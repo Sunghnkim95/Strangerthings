6c272aa8dfec6533cb5cb19bc10a83e26a4bd1f2
Sean <seanc878@yahoo.com>
2021-08-23 16:28:17 -0600

    more work on messages, not functioning yet

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,8 +1,38 @@
 import { useState, useEffect } from "react"
 
+function sendMessage(postId, content){
+    fetch (`https://strangers-things.herokuapp.com/api/2105-VPI-WEB-PT/posts/${postId}/messages`,{
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + userToken
+    },
+    body: JSON.stringify({
+        message: { 
+            'content': content
+        }
+    })
+}).then(result => {
+    console.log(result)
+}).catch(console.error)
+}
+
+const Newmessageform = () => {
+    return <> 
+      <div className="messagetitle">Message Content</div>
+        <input className="messagebody" onchange={function (event){
+            setContent(event.target.value)
+        }}></input>     
+        <button onClick={
+            sendMessage(postId, content)
+        } disabled={content===null}>Send</button>     </>
+}
+
+
+
 const Messages = () => {
     const [messages, setMessages] = useState([])
-    const [recipientID, setRecipientID] = useState('')
+    const [postId, setPostId] = useState('')
     const userToken = localStorage.getItem("token")
     useEffect(() => {
         const fetchMessages = async () => {
@@ -21,45 +51,36 @@ const Messages = () => {
         fetchMessages();
     }, [])
 
-    function sendMessage(recipientID){
-        fetch (`https://strangers-things.herokuapp.com/api/2105-VPI-WEB-PT/posts/${recipientID}/messages`,{
-            method: "POST",
-            headers: {
-                'Content-Type': application/json,
-                'Authorization': 'Bearer ' + userToken
-            },
-            body: JSON.stringify({
-                message: { 
-                    'content': GETCONTENTHERE
-                }
-            })
-        }).then(result => {
-            console.log(result)
-        }).catch(console.error)
-    }
 
     
     return <> 
              <div className="sendandreceived">
             
                      <div className="received">
-                     {messages.map( message => <div key={'something'}>
-                       <div className='title'> From: </div>
-                       <div className='location'> Time: </div>
-                       <div className='description'> Body:  </div>
+                     {messages.map( message => <div key={message._id}>
+                       <div className='username'> From:{message.fromUser.username} </div>
+                       <div className='title'> Title:{message.post.title} </div>
+                       <div className='description'> Body: {message.content} </div>
                        <button onClick={()=> {
-                           setRecipientID(message.author._id)
-                           sendMessage(recipientID)
+                           setPostId(message._id)
                        }}>Reply</button> 
                         </div>)}
                      </div>
-        
-                     <div className="newmessage">
-                        CREATE NEW MESSAGE
-                         <input></input>
+            <div className="nesmessageform">
+            <Newmessageform />
+            </div>
+                     {/* <div className="newmessage">
+                        REPLY MESSAGE
+                        <input className="messageinput"
+                            type="text"
+                            value = {content}
+                            onChange={function(event){
+                                setContent(event.target.value);
+                            }}>
+                            </input> */}
                         
-                         <button>Send</button>
-                     </div>
+                         
+                     {/* </div> */}
         
                     
                  </div>
